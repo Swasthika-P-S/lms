7b483fda55fe93ef44c0648a06a11aab95a7fc87
Swasthika-P-S <swasthikaponnusamy@gmail.com>
2026-01-01 16:16:43 +0530

    added app_tab

--- app_lib/lib/utils/constants.dart
+++
@@ -1,6 +0,0 @@
-class AppConstants {
-  static const String appName = 'LearnHub';
-  static const double borderRadius = 16.0;
-  static const double cardPadding = 16.0;
-  static const double spacing = 16.0;
-}
